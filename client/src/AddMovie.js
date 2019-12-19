ca9d2e78264e6e7b3a989def24f9a10b9e4dc50a
wchamber01 <williamchambers@gmail.com>
2019-12-19 00:52:27 -0500

    built AddMovie.js with form

--- client/src/AddMovie.js
+++ client/src/AddMovie.js
@@ -1,5 +1,48 @@
 import React from "react";
 
-const AddMovie = () => {};
+const AddMovie = () => {
+  return (
+    <div className="addMovie">
+      <form>
+        <input
+          name="title"
+          value=""
+          label="title"
+          id="title"
+          type="text"
+          placeholder="title"
+          onChange={handleChanges}
+        />
+        <input
+          name="director"
+          value=""
+          label="director"
+          id="director"
+          type="text"
+          placeholder="director"
+          onChange={handleChanges}
+        />
+        <input
+          name="metascore"
+          value=""
+          label="metascore"
+          id="metascore"
+          type="text"
+          placeholder="metascore"
+          onChange={handleChanges}
+        />
+        <input
+          name="stars"
+          value=""
+          label="stars"
+          id="stars"
+          type="text"
+          placeholder="stars"
+          onChange={handleChanges}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default AddMovie;
